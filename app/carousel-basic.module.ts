0bcca7802ee47ccf41178a30ba8c7ade70f2874f
KAURHARM0 <harmeet.kaur@sap.com>
2019-07-25 20:54:57 +0530

    2 files modified, 1 added

--- app/carousel-basic.module.ts
+++ app/carousel-basic.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { EventEmitterService } from '../providers/event-emitter.service';
+import { ControlsMaterialModule } from '../material-module';
 
 import { CartValueComponent } from '../templates/cartValueCondition/cartValueCondition.component';
 import { ScrollCarouselComponent } from '../scroll-carousel/scroll-carousel.component';
@@ -11,21 +12,21 @@ import { ConditionListComponent } from '../conditionList-sidePanel/conditionList
 import { RuleDefinitionComponent } from '../rule-definition-content/rule-definition-content.component';
 import { RuleExpressionComponent } from '../rule-expression/rule-expression.component';
 
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatInputModule} from '@angular/material/input';
-import {MatListModule} from '@angular/material/list';
-import {A11yModule} from '@angular/cdk/a11y';
-import {DragDropModule} from '@angular/cdk/drag-drop';
-import {PortalModule} from '@angular/cdk/portal';
-import {ScrollingModule} from '@angular/cdk/scrolling';
-import {CdkStepperModule} from '@angular/cdk/stepper';
-import {CdkTableModule} from '@angular/cdk/table';
-import {CdkTreeModule} from '@angular/cdk/tree';
+// import {MatSidenavModule} from '@angular/material/sidenav';
+// import {MatButtonModule} from '@angular/material/button';
+// import {MatIconModule} from '@angular/material/icon';
+// import {MatToolbarModule} from '@angular/material/toolbar';
+// import {MatGridListModule} from '@angular/material/grid-list';
+// import {MatDividerModule} from '@angular/material/divider';
+// import {MatInputModule} from '@angular/material/input';
+// import {MatListModule} from '@angular/material/list';
+// import {A11yModule} from '@angular/cdk/a11y';
+// import {DragDropModule} from '@angular/cdk/drag-drop';
+// import {PortalModule} from '@angular/cdk/portal';
+// import {ScrollingModule} from '@angular/cdk/scrolling';
+// import {CdkStepperModule} from '@angular/cdk/stepper';
+// import {CdkTableModule} from '@angular/cdk/table';
+// import {CdkTreeModule} from '@angular/cdk/tree';
 
 import { NgbdCarouselBasic } from './carousel-basic';
 
@@ -33,19 +34,21 @@ import { NgbdCarouselBasic } from './carousel-basic';
   imports: [BrowserModule, 
     NgbModule, 
     BrowserAnimationsModule, 
-    MatSidenavModule, 
-    MatButtonModule, 
-    MatIconModule,
-    MatToolbarModule,
-    MatListModule, 
-    MatInputModule,
-    MatGridListModule, 
-    MatDividerModule,
-    A11yModule,
-    CdkStepperModule,
-    CdkTableModule,
-    CdkTreeModule,
-    DragDropModule,],
+    ControlsMaterialModule
+    // MatSidenavModule, 
+    // MatButtonModule, 
+    // MatIconModule,
+    // MatToolbarModule,
+    // MatListModule, 
+    // MatInputModule,
+    // MatGridListModule, 
+    // MatDividerModule,
+    // A11yModule,
+    // CdkStepperModule,
+    // CdkTableModule,
+    // CdkTreeModule,
+    // DragDropModule,
+    ],
   declarations: [NgbdCarouselBasic, 
     CartValueComponent,
     ScrollCarouselComponent, 
@@ -54,20 +57,22 @@ import { NgbdCarouselBasic } from './carousel-basic';
     RuleDefinitionComponent, 
     RuleExpressionComponent,],
   // schemas:[CUSTOM_ELEMENTS_SCHEMA],
-  exports: [NgbdCarouselBasic,
-    MatSidenavModule, 
-    MatListModule, 
-    MatInputModule,
-    MatGridListModule, 
-    MatButtonModule, 
-    MatToolbarModule, 
-    MatIconModule, 
-    MatDividerModule,
-    A11yModule,
-    CdkStepperModule,
-    CdkTableModule,
-    CdkTreeModule,
-    DragDropModule,],
+  exports: [NgbdCarouselBasic, 
+    ControlsMaterialModule
+    // MatSidenavModule, 
+    // MatListModule, 
+    // MatInputModule,
+    // MatGridListModule, 
+    // MatButtonModule, 
+    // MatToolbarModule, 
+    // MatIconModule, 
+    // MatDividerModule,
+    // A11yModule,
+    // CdkStepperModule,
+    // CdkTableModule,
+    // CdkTreeModule,
+    // DragDropModule,
+    ],
   entryComponents: [ CartValueComponent ],
   providers: [ EventEmitterService ],
   bootstrap: [ NgbdCarouselBasic ]
